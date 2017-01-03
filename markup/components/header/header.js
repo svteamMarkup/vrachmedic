d35021f7121b503a4f8a6061ef4d2d001a2e118e
Пользователь <>
2017-01-03 19:15:56 +0500

    03.01 evening

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -7,3 +7,15 @@ $(document).on('click','.header-search__mobile',function() {
 	$(this).toggleClass('_active');
 	$('.header-search__wrapper').slideToggle(300);
 })
+$(function() {
+	if($(window).width < 761) {
+		$(document).on('click','.header-menu__item._dropdown',function() {
+			alert('aaa');
+/*			$(this).children('.header-menu-dropdown').css({
+				"visibility": "visible",
+	    		"opacity": "1",
+	    		"display": "block"
+			})*/
+		})
+	}
+})

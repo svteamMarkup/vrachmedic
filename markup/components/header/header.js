7af998e158dd611277799073064e7342d096efce
Пользователь <>
2017-01-04 18:15:01 +0500

    04.01 evening

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -7,15 +7,24 @@ $(document).on('click','.header-search__mobile',function() {
 	$(this).toggleClass('_active');
 	$('.header-search__wrapper').slideToggle(300);
 })
+$(document).on('click','.header-search__icon',function() {
+	$(this).toggleClass('_active');
+	$('.header-search__wrapper').slideToggle(300);
+})
+
 $(function() {
-	if($(window).width < 761) {
+	
 		$(document).on('click','.header-menu__item._dropdown',function() {
-			alert('aaa');
-/*			$(this).children('.header-menu-dropdown').css({
-				"visibility": "visible",
-	    		"opacity": "1",
-	    		"display": "block"
-			})*/
+			if($(window).width() < 761) {
+				$(this).children('.header-menu-dropdown').css({
+					"visibility": "visible",
+		    		"opacity": "1",
+		    		"display": "block",
+		    		"width": "100%",
+		    		"padding-left": "0",
+		    		"padding-right": "0",
+				})
+			}
 		})
-	}
+	
 })
